8d2fc129cc1fa9206521d2bc990ee31a7046bb8b
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-01-22 22:43:15 +0530

    data types in javascript

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello World");
+
+
+let myAge = 18;
+console.log(`My age is ${myAge}`);
+console.log(typeof myAge);
+
+
+let myName="Laxman Gaidhankar";
+console.log(`My name is ${myName}`);
+console.log(typeof myName);
+
+
+let undefinedValue;
+console.log(typeof undefineValue);
+
+
+let nullValue=null;
+console.log(typeof nullValue);
+
+
+let booleanValue=true;
+console.log(typeof booleanValue);
